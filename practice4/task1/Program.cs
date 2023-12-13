91d00964fc3347b7c9ad9ea890c17ab993b42ec5
nkm-01 <matsuk-nikita@rambler.ru>
2023-12-13 15:09:56 +0500

    practice 4 tasks 1 and 2

---
+++ practice4/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Device.Gpio;
+using Iot.Device.RotaryEncoder;
+using Raven.Iot.Device;
+using Raven.Iot.Device.GpioExpander;
+using UnitsNet;
+using UnitsNet.Units;
+
+const int motorPin = 9;
+var pinA = DeviceHelper.WiringPiToBcm(0);
+var pinB = DeviceHelper.WiringPiToBcm(1);
+
+if (DeviceHelper.GetGpioExpanderDevices() is [var settings])
+{
+    var expander = new GpioExpander(settings);
+    var encoder = new ScaledQuadratureEncoder(pinA, pinB, PinEventTypes.Falling, 20);
+    encoder.PulseCountChanged += (_, args) => {
+        var angle = new Angle(args.Value, AngleUnit.Arcminute);
+        expander.WriteAngle(motorPin, angle);
+        Console.WriteLine($"Angle: {angle.Degrees} degrees");
+    };
+}
+else
+{
+    throw new IoTDeviceException("Expander not found!!!");
+}
---
+++ practice4/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Device.Gpio;
+using Iot.Device.RotaryEncoder;
+using Raven.Iot.Device;
+using Raven.Iot.Device.GpioExpander;
+using UnitsNet;
+using UnitsNet.Units;
+
+const int keyPin = 9;
+var pinA = DeviceHelper.WiringPiToBcm(0);
+var pinB = DeviceHelper.WiringPiToBcm(1);
+
+if (DeviceHelper.GetGpioExpanderDevices() is [var settings])
+{
+    var expander = new GpioExpander(settings);
+    var encoder = new ScaledQuadratureEncoder(pinA, pinB, PinEventTypes.Falling,
+        255,
+        1,
+        0,
+        255);
+    expander.SetPwmFrequency(Frequency.FromKilohertz(25));
+    encoder.ValueChanged += (_, args) =>
+    {
+        expander.AnalogWrite(keyPin, Convert.ToInt32(args.Value));
+        Console.WriteLine($"Speed: {args.Value / 2.55}%");
+    };
+}
+else
+{
+    throw new IoTDeviceException("Expander not found!!!");
+}
