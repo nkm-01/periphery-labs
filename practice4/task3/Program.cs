010ae6d92a5348b6ba21e882413484b9b1ded3e2
hmwdln <grishenkonikolay@yandex.ru>
2023-12-18 16:10:41 +0500

    fixed tasks

--- practice4/task3/Program.cs
+++ practice4/task3/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Device.Gpio;
-using System.Net.WebSockets;
-using Iot.Device.RotaryEncoder;
-using Raven.Iot.Device;
-using Raven.Iot.Device.GpioExpander;
-using UnitsNet;
-using UnitsNet.Units;
-
+﻿
 if (Iot.Device.OneWire.OneWireThermometerDevice.EnumerateDevices().Take(1).ToList() is [var settings])
 {
     while (true)
     {
         var temperature = settings.ReadTemperature(); 
-        Console.WriteLine($"Temperature in °C: {temperature.DegreesCelsius}");
+        Console.WriteLine($"Temperature: {temperature.DegreesCelsius:F}");
         Thread.Sleep(1000);
+    }
+}
+
+while (true)
+{
+    foreach (var term in Iot.Device.OneWire.OneWireThermometerDevice.EnumerateDevices())
+    { 
+        var temperature = term.ReadTemperature(); 
+        Console.WriteLine($"Temperature: {temperature.DegreesCelsius:F}");
+        await Task.Delay(1000);
     }
 }
