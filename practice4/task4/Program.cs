2f1a270670c4f07ecd75f7ced2436e351569fa01
hmwdln <grishenkonikolay@yandex.ru>
2023-12-18 16:11:18 +0500

    still not ready

---
+++ practice4/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Device.Gpio;
+using Iot.Device.RotaryEncoder;
+using OfficeOpenXml.Core.ExcelPackage;
+using Raven.Iot.Device;
+using Raven.Iot.Device.GpioExpander;
+using Raven.Iot.Device.Ina219;
+using UnitsNet;
+using UnitsNet.Units;
+
+
+var pinA = DeviceHelper.BcmToWiringPi(0);
+var pinB = DeviceHelper.BcmToWiringPi(1);
+
+if (DeviceHelper.GetIna219Devices() is [var settings])
+{
+    var calibrator = Ina219Calibrator.Default with
+    {
+        VMax = ElectricPotential.FromVolts(5.0),
+        IMax = ElectricCurrent.FromAmperes(0.6)
+    };
+
+    var ina219 = calibrator.CreateCalibratedDevice(settings);
+    var package = new ExcelPackage();
+
+}
